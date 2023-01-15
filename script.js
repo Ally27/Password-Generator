d9928eaccad65b73fe1749d6c86c9ac738a58074
Allyson Gonzales <Allysonmg21@gmail.com>
2023-01-14 23:42:03 -0800

    testing if statements

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 // Assignment code here
-var specialCharacters = "!@#$%^&*()";
-var numbers = "0123456789";
-var lowerCase = "";
-var upperCase = "";
-var minimumCount = 8;
-var maximumCount = 128;
+var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '-', '.', '~', '|', '<', '>', '=', '-', '_', '/', ':', ';', '?', '[', ']', '{', '}', '~'];
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var characterSetNames = [
+    "specialCharacters",
+    "numbers",
+    "lowerCase",
+    "upperCase",
+];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -17,35 +22,46 @@ function writePassword() {
     passwordText.value = password;
 
 }
+function writePassword() {
+    var password = prompt("How many characters would you like? (8-128)");
+    if (password < 8) {
+        alert("Input must be between 8-128!");
+        password();
 
-function generatePassword() {
-    var passwordLength = prompt("How many characters would you like? (8-128)");
-    var newPassword = ""
-    if (passwordLength) {
-        var number = confirm("Numbers?");
-        if (number) {
-            newPassword += "1"
-
-
-        }
-        var lowercase = confirm("Any lower cases?");
-        if (lowerCase) {
-            newPassword += "a"
-
-        }
-        var uppercase = confirm("What about UPPERCASE?")
-        if (upperCase) {
-            newPassword += "A"
-
-        }
-        var special = confirm("lastly, any special characters?")
-        if (specialCharacters) {
-            newPassword += "!"
-
-        }
+    } else if (password > 128) {
+        alert("Input must be between 8-128!");
+        password();
     }
-    return newPassword
-};
+
+    var numbers = confirm("Numbers?");
+    if (numbers === true) {
+        password += "1";
+
+
+    }
+    var lowerCase = confirm("Any lower cases?");
+    if (lowerCase) {
+        password += "a";
+
+    }
+    var upperCase = confirm("What about UPPERCASE?");
+    if (upperCase) {
+        password += "A";
+
+    }
+    var specialCharacters = confirm("Special characters?");
+    if (specialCharacters) {
+        password += specialCharacters;
+
+    }
+
+    for (var i = 0; i < characterSetNames; i++) {
+        password += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+
+    return password;
+}
+
 
 
 // Add event listener to generate button
