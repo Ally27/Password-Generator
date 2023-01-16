84d4d3360d0c2c25c63228236c0581777c25dc56
Allyson Gonzales <Allysonmg21@gmail.com>
2023-01-15 16:40:33 -0800

    Corrected if statements.

--- script.js
+++ script.js
@@ -1,67 +1,53 @@
 // Assignment code here
+var characterLength = 8;
+var choice = [];
+
 var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '-', '.', '~', '|', '<', '>', '=', '-', '_', '/', ':', ';', '?', '[', ']', '{', '}', '~'];
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-var characterSetNames = [
-    "specialCharacters",
-    "numbers",
-    "lowerCase",
-    "upperCase",
-];
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
-    var password = generatePassword();
+    var newPassword = generatePassword();
     var passwordText = document.querySelector("#password");
 
-    passwordText.value = password;
-
+    passwordText.value = newPassword;
 }
+
 function generatePassword() {
-    var password = prompt("How many characters would you like? (8-128)");
-    if (password < 8) {
-        alert("Input must be between 8-128!");
-        password();
+    var password = "";
+    choice = [];
 
-    } else if (password > 128) {
-        alert("Input must be between 8-128!");
-        password();
+   characterLength = prompt("Enter character length amount. (8-128)");
+   if (isNaN(characterLength) || characterLength <8 || characterLength > 128){
+    alert ("Invalid input. Characters must be between 8-128!");
+    return "";
+   }
+    if (confirm("Numbers?")){
+        choice = choice.concat(numbers);
+    }
+    if (confirm("Lowercases?")){
+        choice = choice.concat(lowerCase);
+    }
+    if (confirm("Uppercases?")){
+        choice = choice.concat(upperCase);
+    }
+    if (confirm("Special Characters?")){
+        choice = choice.concat(specialCharacters);
     }
 
-    var numbers = confirm("Numbers?");
-    if (numbers) {
-        password += "1";
-
-
-    }
-    var lowerCase = confirm("Any lower cases?");
-    if (lowerCase) {
-        password += "a";
-
-    }
-    var upperCase = confirm("What about UPPERCASE?");
-    if (upperCase) {
-        password += "A";
-
-    }
-    var specialCharacters = confirm("Special characters?");
-    if (specialCharacters) {
-        password += "!";
-
-    }
-
-    for (var i = 0; i < characterSetNames; i++) {
-        password += characters.charAt(Math.floor(Math.random() * characters.length));
+    for (var i = 0; i < characterLength; i++) {
+        var index = Math.floor(Math.random() * choice.length);
+        password = password + choice[index];
     }
 
     return password;
 }
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
